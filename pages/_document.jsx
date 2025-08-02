783f3a1671dde6fbe5fa4a7c94a3bf3fe7f98c4c
HHM-1999 <hhm639069@gmail.com>
2025-08-02 14:47:45 +0600

    fixed design and add logo static in root file

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -7,6 +7,9 @@ export default function Document() {
         <Html lang="bn">
             <Head >
                 <meta name="robots" content="noindex, nofollow" />
+                <meta property="og:image" content="public/logo-fb.jpg" />
+                <meta property="og:image:width" content="800" />
+                <meta property="og:image:height" content="420" />
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" />
                 <link rel="icon" href="/favicon.ico" />
